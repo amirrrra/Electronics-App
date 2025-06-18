cf630b673dd68cb86971e242c00098ac65bf1e8c
Amira Tarek <amirat737@gmail.com>
2025-06-18 10:04:59 +0300

    Implement Search Cubit method

--- lib/features/search/presentation/cubits/search_cubit.dart
+++ lib/features/search/presentation/cubits/search_cubit.dart
@@ -1,8 +1,16 @@
+import 'package:electronics_app/features/home/data/repos/products_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'search_state.dart';
 
-class SearchCubit extends Cubit<SearchState>{
-  SearchCubit() : super(SearchInitialState());
-  
+class SearchCubit extends Cubit<SearchState> {
+  final ProductsRepo _productsRepo;
+  SearchCubit(this._productsRepo) : super(SearchInitialState());
+
+  Future<void> fetchRelevantProducts(String query) async {
+    var result = await _productsRepo.fetchAllProducts();
+    result.fold(
+      (failure) => emit(SearchFailureState(failure.errMessage)),
+      (products) => emit(SearchSuccessState(products)),
+    );
+  }
 }
